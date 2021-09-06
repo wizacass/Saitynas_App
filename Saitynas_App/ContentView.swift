8f776893717d700dd7bd31ec3f9af5f5a204d6ad
Wizacass <visak.pet0@gmail.com>
2021-09-06 20:46:15 +0300

    Initial Commit

---
+++ Saitynas_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Saitynas_App
+//
+//  Created by Vytenis Petrauskas on 2021-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Saitynas_App/Saitynas_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saitynas_AppApp.swift
+//  Saitynas_App
+//
+//  Created by Vytenis Petrauskas on 2021-09-06.
+//
+
+import SwiftUI
+
+@main
+struct Saitynas_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
